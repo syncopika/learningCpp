2cfccea17087f622819a4730d6822d2626f3594f
Nicholas H <syncopika@users.noreply.github.com>
2017-02-16 19:19:36 -0500

    Create Point.hh

---
+++ cs11projects/lab1/Point.hh
@@ -0,0 +1,33 @@
+// A 3-dimensional point class!
+// Coordinates are double-precision floating point.
+class Point {
+
+private:
+  double x_coord;
+  double y_coord;
+  double z_coord;
+
+public:
+  // Constructors
+  Point();                      // default constructor
+  Point(double x, double y, double z);    // three-argument constructor
+
+  // Destructor
+  ~Point();
+
+  // Mutator methods
+  void setX(double val);
+  void setY(double val);
+  void setZ(double val);
+
+  // Accessor methods 
+  double getX() const;
+  double getY() const;
+  double getZ() const;
+  
+  // distance between three points
+  double distanceTo(const Point &p) const;
+  
+  //equals methods for three points
+  bool equals(Point &other);
+};

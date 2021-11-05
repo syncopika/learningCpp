da2daa7cb14e73f16573f9065fa5d51aac2004d5
nicholas hung <nhung93@outlook.com>
2021-11-04 23:07:11 -0400

    more stuff

--- raytracing/common.h
+++ raytracing/common.h
@@ -2,12 +2,21 @@
 #define COMMON_H
 
 #include <limits>
+#include <cstdlib>
 
 const double infinity = std::numeric_limits<double>::infinity();
 const double pi = 3.1415926535897932385;
 
-double degrees_to_radians(double degrees) {
+inline double degrees_to_radians(double degrees) {
     return (degrees * pi) / 180.0f;
+}
+
+inline double random_double() {
+    return rand() / (RAND_MAX + 1.0);
+}
+
+inline double random_double(double min, double max) {
+    return min + (max - min)*random_double();
 }
 
 #include "Ray.h"
--- sdl-practice/asteroids/main.cpp
+++ sdl-practice/asteroids/main.cpp
@@ -1,29 +1,37 @@
 #include <SDL2/SDL.h>
+#include <math.h>
 #include <cstdlib>
 #include <ctime>
 #include <iostream>
 #include <string>
 
-const int SCREEN_WIDTH = 550;
+#define PI 3.14159265
+
+const int SCREEN_WIDTH = 500;
 const int SCREEN_HEIGHT = 480;
 const int SPRITE_WIDTH = 34;
 const int SPRITE_HEIGHT = 32;
+
+struct Vec2 {
+    float x;
+    float y;
+};
 
 struct Asteroid {
     int x;
     int y;
     bool isSplit; // if the asteroid has been split already (so is it a whole asteroid or fragment)
     float velocity;
-    float angle; // direction asteroid is facing
+    Vec2 direction; // direction of asteroid
     SDL_Texture* sprite;
 };
 
 struct Player {
     int x;
     int y;
+    int score;
     float velocity;
-    float angle; // direction player is facing
-    int points;
+    Vec2 direction; // direction player is facing
     SDL_Texture* sprite;
 };
 
@@ -61,7 +69,7 @@ SDL_Texture* loadTexture(const std::string &file, SDL_Renderer *ren){
 	return texture; 
 }
 
-void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, int x, int y){
+void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, int x, int y, double rotation){
 	// set up a destination rectangle to be at the position given by x and y 
 	SDL_Rect dst;
 	dst.x = x;
@@ -71,7 +79,7 @@ void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, int x, int y){
 	// the rectangle will take on the width and height of the texture 
 	SDL_QueryTexture(tex, NULL, NULL, &dst.w, &dst.h);
 	
-	SDL_RenderCopyEx(ren, tex, NULL, &dst, 0, NULL, SDL_FLIP_NONE);
+	SDL_RenderCopyEx(ren, tex, NULL, &dst, rotation, NULL, SDL_FLIP_NONE);
 }
 
 int main(int argc, char** argv){
@@ -112,21 +120,21 @@ int main(int argc, char** argv){
 		return 1;
 	}
 	// put the background on the screen
-	renderTexture(bg, renderer, 0, 0);
+	renderTexture(bg, renderer, 0, 0, 0);
 	
 	/***
 		set up the player
 	***/
-    Player p1{80, 160, 0.0f, 0.0f, 0, nullptr};
-	SDL_Texture* pTex = loadTexture( "playerSprite.bmp", renderer);
+    Player p1{SCREEN_WIDTH/2, SCREEN_HEIGHT/2, 0, 0.0f, {0.f, 0.f}, nullptr};
+	SDL_Texture* pTex = loadTexture("playerSprite.bmp", renderer);
 	if(pTex == nullptr){
 		return 1;
 	}
 	p1.sprite = pTex;
 	
 	// add asteroid sprites
-    Asteroid a1{20, 34, 0.0f, 0.0f, 0, nullptr};
-    Asteroid a2{70, 80, 0.0f, 0.0f, 0, nullptr};    
+    Asteroid a1{20, 34, false, 0.0f, {0.f, 0.f}, nullptr};
+    Asteroid a2{70, 80, false, 0.0f, {1.2f, 1.5f}, nullptr};
 	SDL_Texture* ast1 = loadTexture("asteroidSprite1.bmp", renderer);
 	SDL_Texture* ast2 = loadTexture("asteroidSprite2.bmp", renderer);
 	if(ast1 == nullptr || ast2 == nullptr){
@@ -148,6 +156,8 @@ int main(int argc, char** argv){
 	bool quit = false; 
 	SDL_Event event;
 	//const Uint8 *keystate;
+    
+    double angle = 0;
 	
 	while(!quit){
 		// check for actions that will quit the program 
@@ -174,13 +184,22 @@ int main(int argc, char** argv){
 		//keystate = SDL_GetKeyboardState(NULL);
 	
 		// redraw the background
-		//SDL_Delay(50);
-		renderTexture(bg, renderer, 0, 0);
+		renderTexture(bg, renderer, 0, 0, 0);
 		
 		// redraw the sprites
-        renderTexture(p1.sprite, renderer, p1.x, p1.y);
-        renderTexture(a1.sprite, renderer, a1.x, a1.y);
-        renderTexture(a2.sprite, renderer, a2.x, a2.y);
+        angle = (angle == 360 ? 0 : angle+1);
+        renderTexture(p1.sprite, renderer, p1.x, p1.y, angle);
+        
+        renderTexture(a1.sprite, renderer, a1.x, a1.y, 0);
+
+        // move asteroid
+        a2.x += a2.direction.x;
+        a2.y += a2.direction.y;
+        if(a2.y > SCREEN_HEIGHT || a2.x > SCREEN_WIDTH){
+            a2.y = -a2.direction.y;
+            a2.x = -a2.direction.x;
+        }
+        renderTexture(a2.sprite, renderer, a2.x, a2.y, -angle);
 		
 		// update screen 
 		SDL_RenderPresent(renderer);

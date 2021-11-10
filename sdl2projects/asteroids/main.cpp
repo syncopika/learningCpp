ab054a7a16f89b54a5d135be8326b3978a40394b
nicholas hung <nhung93@outlook.com>
2021-11-09 20:23:37 -0500

    more stuff! also wrote a ppm viewer that can be used with the raytracing stuff

--- sdl2projects/asteroids/main.cpp
+++ sdl2projects/asteroids/main.cpp
@@ -45,7 +45,7 @@ struct Player {
 };
 
 void logSDLError(std::ostream &os, const std::string &msg){
-	os << msg << " error: " << SDL_GetError() << std::endl;
+    os << msg << " error: " << SDL_GetError() << std::endl;
 }
 
 Direction determineMovement(const Uint8* state){
@@ -171,46 +171,46 @@ void movePlayer(Player& p, Direction dir){
 }
 
 SDL_Texture* loadTexture(const std::string &file, SDL_Renderer *ren){
-	// initialize texture to null first 
-	SDL_Texture *texture = nullptr;
-	
-	// load the image 
-	SDL_Surface *loadedImage = SDL_LoadBMP(file.c_str());
-	
-	// if the load was successful, loadedImage won't be nullptr 
-	if(loadedImage != nullptr){
-		// color key the surface to make any 255,255,255 pixels transparent!
-		 SDL_SetColorKey(loadedImage, SDL_TRUE, SDL_MapRGB(loadedImage->format, 0xFF, 0xFF, 0xFF ) );
-		
-		// turn the surface to a texture 
-		texture = SDL_CreateTextureFromSurface(ren, loadedImage);
-		
-		// free loadedImage memory since we're done with it 
-		SDL_FreeSurface(loadedImage);
-		
-		// make sure texture creation was successful 
-		if(texture == nullptr){
-			logSDLError(std::cout, "CreateTextureFromSurface");
-		}
-	}else{
-		// error with loadedImage 
-		logSDLError(std::cout, "LoadBMP");
-	}
-	
-	return texture; 
+    // initialize texture to null first 
+    SDL_Texture *texture = nullptr;
+    
+    // load the image 
+    SDL_Surface *loadedImage = SDL_LoadBMP(file.c_str());
+    
+    // if the load was successful, loadedImage won't be nullptr 
+    if(loadedImage != nullptr){
+        // color key the surface to make any 255,255,255 pixels transparent!
+         SDL_SetColorKey(loadedImage, SDL_TRUE, SDL_MapRGB(loadedImage->format, 0xFF, 0xFF, 0xFF ) );
+        
+        // turn the surface to a texture 
+        texture = SDL_CreateTextureFromSurface(ren, loadedImage);
+        
+        // free loadedImage memory since we're done with it 
+        SDL_FreeSurface(loadedImage);
+        
+        // make sure texture creation was successful 
+        if(texture == nullptr){
+            logSDLError(std::cout, "CreateTextureFromSurface");
+        }
+    }else{
+        // error with loadedImage 
+        logSDLError(std::cout, "LoadBMP");
+    }
+    
+    return texture; 
 }
 
 void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, int x, int y, double rotation){
-	// set up a destination rectangle to be at the position given by x and y 
-	SDL_Rect dst;
-	dst.x = x;
-	dst.y = y;
-	
-	// query the texture to get the width and height 
-	// the rectangle will take on the width and height of the texture 
-	SDL_QueryTexture(tex, nullptr, nullptr, &dst.w, &dst.h);
-	
-	SDL_RenderCopyEx(ren, tex, nullptr, &dst, rotation, nullptr, SDL_FLIP_NONE);
+    // set up a destination rectangle to be at the position given by x and y 
+    SDL_Rect dst;
+    dst.x = x;
+    dst.y = y;
+    
+    // query the texture to get the width and height 
+    // the rectangle will take on the width and height of the texture 
+    SDL_QueryTexture(tex, nullptr, nullptr, &dst.w, &dst.h);
+    
+    SDL_RenderCopyEx(ren, tex, nullptr, &dst, rotation, nullptr, SDL_FLIP_NONE);
 }
 
 void moveAsteroid(Asteroid* asteroid){
@@ -346,75 +346,75 @@ void handleCollisions(Player& p, std::vector<Asteroid*>& asteroids){
 }
 
 int main(int argc, char** argv){
-	if(SDL_Init(SDL_INIT_VIDEO) != 0){
-		logSDLError(std::cout,"SDL_Init Error: ");
-		return 1;
-	}
-	
-	// create a window 
-	SDL_Window *window = SDL_CreateWindow("asteroids", 100, 100, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-	if(window == nullptr){
-		logSDLError(std::cout, "SDL_CreateWindow Error: ");
-		SDL_Quit();
-		return 1;
-	}
-	
-	// create the renderer to render the window with 
-	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-	if(renderer == nullptr){
-		SDL_DestroyWindow(window);
-		logSDLError(std::cout,"SDL_CreateRenderer Error: ");
-		SDL_Quit();
-		return 1;
-	}
-	
-	// initialize random seed
-	srand(time(nullptr));
-	
-	/***
-		set up background
-	***/
-	SDL_Texture *bg = loadTexture("background.bmp", renderer);
-	if(bg == nullptr){
-		SDL_DestroyWindow(window);
-		SDL_DestroyRenderer(renderer);
-		logSDLError(std::cout,"BG creation failed: ");
-		SDL_Quit();
-		return 1;
-	}
-	// put the background on the screen
-	renderTexture(bg, renderer, 0, 0, 0);
-	
-	/***
-		set up the player
-	***/
+    if(SDL_Init(SDL_INIT_VIDEO) != 0){
+        logSDLError(std::cout,"SDL_Init Error: ");
+        return 1;
+    }
+    
+    // create a window 
+    SDL_Window *window = SDL_CreateWindow("asteroids", 100, 100, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    if(window == nullptr){
+        logSDLError(std::cout, "SDL_CreateWindow Error: ");
+        SDL_Quit();
+        return 1;
+    }
+    
+    // create the renderer to render the window with 
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    if(renderer == nullptr){
+        SDL_DestroyWindow(window);
+        logSDLError(std::cout,"SDL_CreateRenderer Error: ");
+        SDL_Quit();
+        return 1;
+    }
+    
+    // initialize random seed
+    srand(time(nullptr));
+    
+    /***
+        set up background
+    ***/
+    SDL_Texture *bg = loadTexture("background.bmp", renderer);
+    if(bg == nullptr){
+        SDL_DestroyWindow(window);
+        SDL_DestroyRenderer(renderer);
+        logSDLError(std::cout,"BG creation failed: ");
+        SDL_Quit();
+        return 1;
+    }
+    // put the background on the screen
+    renderTexture(bg, renderer, 0, 0, 0);
+    
+    /***
+        set up the player
+    ***/
     Player p1{SCREEN_WIDTH/2, SCREEN_HEIGHT/2, 0, 0, 0.f, {0.f, -1.f}, nullptr};
-	SDL_Texture* pTex = loadTexture("playerSprite.bmp", renderer);
-	if(pTex == nullptr){
-		return 1;
-	}
-	p1.sprite = pTex;
-	
-	// add asteroid sprites
+    SDL_Texture* pTex = loadTexture("playerSprite.bmp", renderer);
+    if(pTex == nullptr){
+        return 1;
+    }
+    p1.sprite = pTex;
+    
+    // add asteroid sprites
     Asteroid a1{50, 44, 20, false, {-1.5f, 1.6f}, nullptr};
     Asteroid a2{70, 80, 10, false, {1.8f, 1.8f}, nullptr};
     Asteroid a3{90, 280, 25, false, {-1.4f, -1.5f}, nullptr};
     
-	SDL_Texture* ast1 = loadTexture("asteroidSprite1.bmp", renderer);
-	SDL_Texture* ast2 = loadTexture("asteroidSprite2.bmp", renderer);
-	if(ast1 == nullptr || ast2 == nullptr){
-		SDL_DestroyWindow(window);
-		SDL_DestroyRenderer(renderer);
-		logSDLError(std::cout,"asteroid sprite creation failed: ");
-		SDL_Quit();
-		return 1;
-	}
+    SDL_Texture* ast1 = loadTexture("asteroidSprite1.bmp", renderer);
+    SDL_Texture* ast2 = loadTexture("asteroidSprite2.bmp", renderer);
+    if(ast1 == nullptr || ast2 == nullptr){
+        SDL_DestroyWindow(window);
+        SDL_DestroyRenderer(renderer);
+        logSDLError(std::cout,"asteroid sprite creation failed: ");
+        SDL_Quit();
+        return 1;
+    }
     a1.sprite = ast1;
     a2.sprite = ast2;
     a3.sprite = ast1;
 
-	// show on screen 
-	SDL_RenderPresent(renderer);
+    // show on screen 
+    SDL_RenderPresent(renderer);
     
     // add asteroids to a queue so we can process them in the event loop
     // using a queue to easily handle many of them
@@ -422,53 +422,62 @@ int main(int argc, char** argv){
     asteroids.push_back(&a1);
     asteroids.push_back(&a2);
     asteroids.push_back(&a3);
-	
-	/***		
-		BEGIN EVENT LOOP 
-	***/
-	bool quit = false; 
-	SDL_Event event;
-	const Uint8 *keystate;
-	
-	while(!quit){
-		// check for actions that will quit the program 
-		if(SDL_PollEvent(&event)){
-			switch(event.type){
-				/* clicking on the X button on top right of screen */
-				case SDL_QUIT:
-					quit = true;
-					break;
-				
-				/* pressing q or Esc on keyboard and handles movement for player */
+    
+    /***        
+        BEGIN EVENT LOOP 
+    ***/
+    bool quit = false; 
+    SDL_Event event;
+    const Uint8 *keystate;
+    
+    while(!quit){
+        // check for actions that will quit the program 
+        if(SDL_PollEvent(&event)){
+            switch(event.type){
+                /* clicking on the X button on top right of screen */
+                case SDL_QUIT:
+                    quit = true;
+                    break;
+                
+                /* pressing q or Esc on keyboard and handles movement for player */
                 // we're avoiding SDL_GetKeyboardState because that registers multiples events for a single button press
                 // which we don't want in this case
-				case SDL_KEYDOWN:
-					switch (event.key.keysym.sym){
-						case SDLK_ESCAPE:
-						case SDLK_q:
-							quit = true;
-							break;
+                case SDL_KEYDOWN:
+                    switch (event.key.keysym.sym){
+                        case SDLK_ESCAPE:
+                        case SDLK_q:
+                            quit = true;
+                            break;
                         case SDLK_LEFT:
                             movePlayer(p1, LEFT);
                             break;
                         case SDLK_RIGHT:
                             movePlayer(p1, RIGHT);
                             break;
-					}
-				break;
-			}
-		}
+                    }
+                break;
+            }
+        }
         // redraw the background
-		renderTexture(bg, renderer, 0, 0, 0);
+        renderTexture(bg, renderer, 0, 0, 0);
         
-		// then handle sprite movement
-		keystate = SDL_GetKeyboardState(NULL);
-		
-		// redraw the sprites
+        // then handle sprite movement
+        keystate = SDL_GetKeyboardState(NULL);
+        
+        // redraw the sprites
         //angle = (angle == 360 ? 0 : angle+1);
         Direction dir = determineMovement(keystate);
         if(dir == UP || dir == NONE){
             movePlayer(p1, dir);
+            if(p1.y > SCREEN_HEIGHT){
+                p1.y = -30;
+            }else if(p1.x > SCREEN_WIDTH){
+                p1.x = -30;
+            }else if(p1.y < -50){
+                p1.y = SCREEN_HEIGHT-10; 
+            }else if(p1.x < -50){
+                p1.x = SCREEN_WIDTH-10;
+            }
         }
         
         double currAngle = (atan2(p1.forward.y, p1.forward.x) * 180) / PI;
@@ -480,16 +489,16 @@ int main(int argc, char** argv){
             moveAsteroid(ast);
             renderTexture(ast->sprite, renderer, ast->x, ast->y, 0);
         }
-		
-		// update screen 
-		SDL_RenderPresent(renderer);
-	}
-	
-	//cleanup
+        
+        // update screen 
+        SDL_RenderPresent(renderer);
+    }
+    
+    //cleanup
     SDL_DestroyTexture(bg);
     SDL_DestroyTexture(pTex);
     SDL_DestroyTexture(ast1);
     SDL_DestroyTexture(ast2);
-	SDL_Quit();
-	return 0;
+    SDL_Quit();
+    return 0;
 }
